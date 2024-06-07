1833e16b2deb8fcda0be9f77f5f4ae9f9e99ff6b
tini <mart1na.pl3tz@gmail.com>
2024-06-07 16:04:40 +0200

    Add working Views

--- Controllers/Song.php
+++ Controllers/Song.php
@@ -1,12 +1,19 @@
 <?php
 
 class Controllers_Song extends Controllers_Base{
+    private $model;
 
     public function __construct(Views_Base $view, array $params){
         parent::__construct($view, $params);
+        $this->model = new Models_Song();
     }
 
-    public function get(){
-        $this->view->render(2);
+    public function get() {
+        if ($this->params) {
+            $data = $this->model->findById($this->params[0]);
+        } else {
+            $data = $this->model->findAll();
+        }
+        $this->view->render($data);
     }
 }
