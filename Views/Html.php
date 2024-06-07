f71a36b8963095a4b31d42b5f42808740327c84a
tini <mart1na.pl3tz@gmail.com>
2024-06-07 15:43:10 +0200

    Add Views

--- Views/Html.php
+++ Views/Html.php
@@ -2,9 +2,15 @@
 
 class Views_Html extends Views_Base{
 
-
     public function render($data){
-        $template = "helloWorld.phtml";
+
+        if(is_array($data)) {
+            $template = "table.phtml";
+        } else {
+            $template = "object.phtml";
+        }    
+
+        # $template = "helloWorld.phtml";
         if (is_readable(dirname("__FILE__") . "/templates/") . strtolower($this->resource_name) . "/" . $template) {
             $template = strtolower($this->resource_name) . "/" . $template;
         }
